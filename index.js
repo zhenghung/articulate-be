e52f6d4ad1a103c42d3d63e8b8021de6ab6618fb
Zheng Hung Chuah <zheng_hung@hotmail.com>
2020-04-28 19:27:21 +0100

    Use soba-be

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const soba = require('soba-be');
 
 const router = require('./router');
 
@@ -14,53 +15,7 @@ app.use(express.json());
 app.use(router);
 
 io.on('connect', (socket) => {
-
-    /** Client requesting a SocketId,
-     * sent back to client */
-    socket.on('getSocketId', ({}, callback) => {
-        socket.emit('socketId', {socketId: socket.id});
-        callback();
-    });
-
-    /** Client requesting to join a room,
-     * sends to everyone in the room */
-    socket.on('joinRoom',
-        ({isHost, playerName, socketId, roomCode}, callback) => {
-            console.log('JOINING ROOM: ', {playerName, socketId, roomCode});
-            socket.join(roomCode);
-            socket.emit('socketId', {socketId: socket.id});
-            if (!isHost && io.sockets.adapter.rooms[roomCode].length < 2) {
-                socket.leave(roomCode);
-                socket.emit('playerJoinedFailed', {playerName, message: "Invalid Room"});
-            } else {
-                io.in(roomCode).emit('playerJoined', {isHost, playerName, socketId: socket.id});
-            }
-            callback();
-        });
-
-    /** Host rejects player joining,
-     * Sends to everyone in the room but only acted upon by target player */
-    socket.on('rejectPlayer', ({roomCode, playerName}, callback) => {
-        socket.to(roomCode).emit('playerRejected', {roomCode, playerName});
-        callback();
-    });
-
-    /** Rejected player leaves room */
-    socket.on('leaveRoom', (roomCode, callback) => {
-        socket.leave(roomCode);
-        callback();
-    });
-
-    /** Client broadcasting their gameState,
-     * sends to everyone else in the same room */
-    socket.on('broadcastGameState', (gameState, callback) => {
-        // console.log(gameState);
-        if (gameState.hasOwnProperty('roomCode')) {
-            console.log('BROADCASTING GAME STATE: ', gameState.roomCode);
-            socket.to(gameState.roomCode).emit('updateGameState', gameState);
-        }
-        callback();
-    });
+    soba(io, socket, {logging: true});
 
     /** Client presses correct or skip during game,
      * sends to everyone else in the same room */
